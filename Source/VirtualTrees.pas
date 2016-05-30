922dc6047329c862247b047247a38606faad4da8
Luiz Américo <brandkarl@gmail.com>
2016-05-30 13:24:28 -0300

    Fix selection draw in empty areas when header is visible

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -30559,6 +30559,9 @@ begin
           if DrawSelectionRect then
           begin
             R := OrderRect(FNewSelRect);
+            //lclheader
+            if hoVisible in FHeader.Options then
+              OffsetRect(R, 0, FHeader.Height);
             // Remap the selection rectangle to the current window of the tree.
             // Since Target has been used for other tasks BaseOffset got the left extent of the target position here.
             OffsetRect(R, -Target.X + BaseOffset - Window.Left, -Target.Y + FOffsetY);
